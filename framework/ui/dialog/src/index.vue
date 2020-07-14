2877ce6a0c54c157edb444a215b5efc438da47a3
lukunkun <lukunkun@ztgame.com>
2020-07-14 10:32:49 +0800

    增加dialog公共ui组件

---
+++ framework/ui/dialog/src/index.vue
@@ -0,0 +1,162 @@
+<template>
+  <el-dialog
+    :class="['gt-dialog', currentTheme]"
+    v-bind="$attrs"
+    :visible="visible"
+    :top="top"
+    :before-close="handleClose"
+    @open="this.$emit('open')"
+    @opened="this.$emit('opened')"
+    @close="this.$emit('close')"
+    @closed="this.$emit('closed')"
+  >
+    <div slot="title" class="gt-dialog__title">
+      <template v-if="title">{{ title }}</template>
+      <solt v-else name="title" />
+    </div>
+    <div v-loading="isLoading" class="gt-dialog__body" :style="{ maxHeight: _maxHeight }">
+      <slot />
+    </div>
+    <slot v-if="isShowFooter" slot="footer" class="gt-dialog__footer" name="footer">
+      <el-button @click="handleClose">取消</el-button>
+      <el-button @click="handleSave" type="primary">确定</el-button>
+    </slot>
+  </el-dialog>
+</template>
+<script>
+export default {
+  name: 'GtDialog',
+  data() {
+    return {};
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    theme: {
+      type: String,
+      default: 'primary'
+    },
+    mode: {
+      type: String,
+      default: 'middle'
+    },
+    maxHeight: {
+      type: String,
+      default: ''
+    },
+    isShowFooter: {
+      type: Boolean,
+      default: true
+    },
+    top: {
+      type: String,
+      default: '10vh'
+    },
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    currentTheme() {
+      const cla = 'gt-dialog--theme gt-dialog--';
+      switch (this.theme) {
+        case 'primary':
+          return cla + this.theme;
+        default:
+          return '';
+      }
+    },
+    _maxHeight() {
+      if (this.maxHeight) {
+        const res = this.parseStyle(this.maxHeight);
+        if (res.value > 0) {
+          return res.value + res.unit;
+        }
+      }
+      if (this.mode === 'middle') {
+        const top = this.parseStyle(this.top);
+        const other = this.isShowFooter ? 142 : 83;
+        return `calc(100vh - ${top.value * 2}${top.unit} - ${other}px)`;
+      }
+      return '';
+    }
+  },
+  methods: {
+    parseStyle(str) {
+      const reg = /px|vh|%$/;
+      const res = str.match(reg);
+      const data = {
+        value: 0,
+        unit: 'px'
+      };
+      let number = 0;
+      if (res) {
+        data.unit = res['0'];
+        number = Number(str.substr(0, res.index));
+      } else {
+        number = Number(str);
+      }
+      if (number) data.value = number;
+      return data;
+    },
+    handleClose() {
+      if (typeof this.beforeClose === 'function') {
+        this.beforeClose();
+      }
+      this.$emit('close');
+      this.$emit('update:visible', false);
+    },
+    handleSave() {
+      this.$emit('save');
+      this.$emit('update:visible', false);
+    }
+  }
+};
+</script>
+<style lang="scss">
+$primary: #409eff;
+.gt-dialog {
+    &.gt-dialog--theme {
+        /deep/ .el-dialog__header {
+            padding: 0 20px;
+        }
+        .gt-dialog__title {
+            line-height: 42px;
+        }
+    }
+    &.gt-dialog--primary {
+        /deep/ .el-dialog__header {
+            background-color: #F8F8F8;
+            border-bottom: 1px solid #eee;
+        }
+        /deep/ .el-dialog__headerbtn {
+            top: 11px;
+            right: 11px;
+            .el-dialog__close {
+                color: #333;
+                &:hover {
+                    color: $primary;
+                }
+            }
+        }
+        /deep/ .el-dialog__footer {
+            padding: 15px 20px;
+        }
+    }
+     /deep/ .el-dialog__body {
+        padding: 0;
+    }
+    .gt-dialog__body {
+        padding: 20px;
+        min-height: 100px;
+        overflow-y: auto;
+    }
+}
+</style>
