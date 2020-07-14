3c38b39a9716b4602c26c375c7dc8b541c696232
caoshitong <caoshitong@ztgame.com>
2020-07-14 18:58:32 +0800

    modify eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
     browser: true,
     node: true,
   },
-  
   // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
   // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
   extends: ['plugin:vue/strongly-recommended', 'airbnb-base'],
